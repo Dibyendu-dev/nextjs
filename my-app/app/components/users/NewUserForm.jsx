8de5ffa0bd54c70d9e226553586e3d4e1932f4bc
Dibyendu Das <98630356+Dibyendu-dev@users.noreply.github.com>
2024-08-09 10:51:40 +0000

    mongodb connection is established

--- my-app/app/components/users/NewUserForm.jsx
+++ my-app/app/components/users/NewUserForm.jsx
@@ -1,6 +1,15 @@
 export default function NewUserForm() {
+
+    const addUser = async (formData)=>{
+        "use server";
+
+        console.log(formData.get('name'));
+        console.log(formData.get('email'));
+
+    }
+
     return (
-        <form >
+        <form action={addUser} >
             <div>
                 <input type="text" name="name" placeholder="Name" />
             </div>
